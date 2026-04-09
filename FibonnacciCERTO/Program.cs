f0d0d31736e76f1a2b0e92b28f9740d12cede8a7
unknown <dev@NSN1191261228.edu119.local>
2026-04-09 14:52:15 -0300

    Fibonacci

---
+++ FibonnacciCERTO/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Fibonnacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreva aqui um limite para a sua sequência fibonacci: ");
+            int.TryParse(Console.ReadLine(), out int limite);
+            int termos, t1 = 0, t2 = 2, t3 = 4;
+            for (int i = 0; i < limite; i++)
+            {
+                t3 = t1 + t2;
+                t1 = t2;
+                t2 = t3;
+                Console.Write($"{t3} --> ");
+            }
+        }
+    }
+}
